8ff6be637f0359bbc7ad06613bd3b264babf0989
sanjana2296 <sanjana.poojary22@gmail.com>
2025-03-22 10:31:22 -0400

    slack messages

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -1,3 +1,6 @@
+
+
+
 /**
 =========================================================
 * Material Dashboard 2 React - v2.2.0
@@ -31,9 +34,65 @@ import DataTable from "examples/Tables/DataTable";
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
 
+// Gantt Chart Component
+import React from "react";
+import { GanttOriginal, ViewMode } from "react-gantt-chart";
+
 function Tables() {
   const { columns, rows } = authorsTableData();
   const { columns: pColumns, rows: pRows } = projectsTableData();
+
+  // Define the state for tasks in Gantt Chart
+  const [tasks] = React.useState([
+    {
+      type: "project",
+      id: "ProjectSample",
+      name: "1.Project",
+      start: new Date(2021, 6, 1),
+      end: new Date(2021, 9, 30),
+      progress: 25,
+      hideChildren: false,
+    },
+    {
+      type: "task",
+      id: "Task 0",
+      name: "1.1 Task",
+      start: new Date(2021, 6, 1),
+      end: new Date(2021, 6, 30),
+      progress: 45,
+      project: "ProjectSample",
+    },
+    {
+      type: "task",
+      id: "Task 1",
+      name: "1.2 Task",
+      start: new Date(2021, 7, 1),
+      end: new Date(2021, 7, 30),
+      progress: 25,
+      dependencies: ["Task 0"],
+      project: "ProjectSample",
+    },
+    {
+      type: "task",
+      id: "Task 2",
+      name: "1.3 Task",
+      start: new Date(2021, 6, 1),
+      end: new Date(2021, 7, 30),
+      progress: 10,
+      dependencies: ["Task 1"],
+      project: "ProjectSample",
+    },
+    {
+      type: "milestone",
+      id: "Task 6",
+      name: "1.3.1 MileStone (KT)",
+      start: new Date(2021, 6, 1),
+      end: new Date(2021, 6, 30),
+      progress: 100,
+      dependencies: ["Task 2"],
+      project: "ProjectSample",
+    },
+  ]);
 
   return (
     <DashboardLayout>
@@ -90,6 +149,33 @@ function Tables() {
                   entriesPerPage={false}
                   showTotalEntries={false}
                   noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          {/* Gantt Chart */}
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Project Gantt Chart
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <GanttOriginal
+                  tasks={tasks}
+                  viewMode={ViewMode.Month}
+                  columnWidth={200}
+                  ganttHeight={300}
                 />
               </MDBox>
             </Card>
--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,7 @@ Coded by www.creative-tim.com
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
+import SlackMessages from "layouts/tables/SlackMessages";
 import Billing from "layouts/billing";
 import RTL from "layouts/rtl";
 import Notifications from "layouts/notifications";
@@ -64,6 +65,15 @@ const routes = [
     icon: <Icon fontSize="small">table_view</Icon>,
     route: "/tables",
     component: <Tables />,
+  },
+  {
+    type: "collapse",
+    name: "Slack",
+    key: "slack",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/slack",
+    component: 
+    <SlackMessages />,
   },
   {
     type: "collapse",
